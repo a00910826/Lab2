9e3f1c5a24d2702c054e0c3d4a7bf4af18918940
jcsantos23 <jcsantos23@gmail.com>
2016-01-22 17:35:35 -0800

    ci-basic03 finished

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,12 +1,13 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class About extends Application {
-    
-	public function index()
-	{
-		//$this->load->view('about');
-            $this->data['pagebody'] ='about';
-            $this->render();
-	}
+
+    public function index() {
+        //$this->load->view('about');
+        $this->data['pagebody'] = 'about';
+        $this->render();
+    }
+
 }
